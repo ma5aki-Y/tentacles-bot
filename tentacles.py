a037403fd8f4294a94558faaa4cb757405768d7d
ma5aki-y <yamama.dev@gmail.com>
2021-09-21 03:39:22 +0900

    :+1: チーム分け機能

--- tentacles.py
+++ tentacles.py
@@ -1,3 +1,5 @@
+import random
+
 import discord
 from discord.ext import commands
 
@@ -9,9 +11,11 @@ TOKEN = os.environ['BOT_TOKEN']
 
 bot = commands.Bot(command_prefix='/')
 
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+
 
 @bot.command(name='st')
 async def show_stage(ctx, *args):
@@ -21,10 +25,11 @@ async def show_stage(ctx, *args):
     league = stage_info.get_league(ctx)
     salmon = stage_info.get_salmon(ctx)
 
-    if len(args) > 2 :
+    if len(args) >= 2:
         await ctx.channel.send("引数が多いようです...")
+        return
     elif len(args) == 0:
-        embeds = [regular,ranked,league,salmon]
+        embeds = [regular, ranked, league, salmon]
         for embed in embeds:
             await ctx.channel.send(embed=embed)
         return
@@ -44,5 +49,88 @@ async def show_stage(ctx, *args):
     else:
         await ctx.channel.send("引数がおかしいようです...")
         return
+
+
+class ShuffleButtonView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+        self.add_item(ReadyButton())
+        self.add_item(RetryButton())
+
+
+class ReadyButton(discord.ui.Button):
+    def __init__(self):
+        super().__init__(label="これでいく！", style=discord.ButtonStyle.green)
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.message.channel.send("チーム分け完了！ \n", view=ComeBuckButtonView())
+
+
+class RetryButton(discord.ui.Button):
+    def __init__(self):
+        super().__init__(label="もう一回！", style=discord.ButtonStyle.red)
+
+    async def callback(self, interaction: discord.Interaction):
+        if interaction.user.voice is None:
+            await interaction.message.channel.send("ボイスチャンネルに入ってね!")
+            return
+
+        name = list(split_list([member.name for member in interaction.user.voice.channel.members]))
+        await interaction.message.channel.send(":a: " + str(*name[0]) + "\n" + ":b: " + str(*name[1]), view=ShuffleButtonView())
+
+
+class ComeBuckButtonView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+        self.add_item(ComeBuckButton())
+
+
+class ComeBuckButton(discord.ui.Button):
+    def __init__(self):
+        super().__init__(label="戻す", style=discord.ButtonStyle.green)
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.message.channel.send("戻したよ！", view=ReShuffleButtonView())
+
+
+class ReShuffleButtonView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+        self.add_item(ReShuffleButton())
+
+
+class ReShuffleButton(discord.ui.Button):
+    def __init__(self):
+        super().__init__(label="またチーム分けする", style=discord.ButtonStyle.green)
+
+    async def callback(self, interaction: discord.Interaction):
+        if interaction.user.voice is None:
+            await interaction.message.channel.send("ボイスチャンネルに入ってね!")
+            return
+
+        name = list(split_list([member.name for member in interaction.user.voice.channel.members]))
+        await interaction.message.channel.send(":a: " + str(*name[0]) + "\n" + ":b: " + str(*name[1]), view=ShuffleButtonView())
+
+
+def split_list(members):
+    random.shuffle(members)
+    yield members[0:len(members)//2]
+    yield members[len(members)//2:]
+
+
+@bot.event
+async def on_message(message: discord.Message):
+    if message.author.bot:
+        return
+    if message.content.startswith("/team"):
+        if message.author.voice is None:
+            await message.channel.send("ボイスチャンネルに入ってね!")
+            return
+        name = list(split_list([member.name for member in message.author.voice.channel.members]))
+        await message.channel.send(":a: " + str(*name[0]) + "\n" + ":b: " + str(*name[1]), view=ShuffleButtonView())
+
 
 bot.run(TOKEN)
